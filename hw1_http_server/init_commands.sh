4a824b8592abb48543cfa46d58a10bffaa77188f
vecherninanika <nikavecher333@gmail.com>
2023-03-23 09:44:45 +0300

    tests corrected

--- hw1_http_server/init_commands.sh
+++ hw1_http_server/init_commands.sh
@@ -2,7 +2,7 @@
 
 pip install -r requirements/requirements.txt
 
-docker run -d --name names_hw1 -p 5436:5432 \
+docker run -d --name names_hw1 -p 5432:5432 \
  -v $HOME/DATABASES/names_hw1:/var/lib/postresql/names_hw1 \
  -e POSTGRES_PASSWORD=change_me \
  -e POSTGRES_USER=sirius_2023 \
@@ -11,4 +11,4 @@ docker run -d --name names_hw1 -p 5436:5432 \
 
 sleep 2
 
-psql -h 127.0.0.1 -p 5436 -U sirius_2023 -d names_hw1 -f init_commands.ddl
+psql -h 127.0.0.1 -p 5432 -U sirius_2023 -d names_hw1 -f init_commands.ddl
